9307b72f0b3e54b981328f99826b321e7b52d19c
ashwathisugathan <72198688+ashwathisugathan@users.noreply.github.com>
2020-10-16 21:35:52 +0530

    Update struct.c

--- struct.c
+++ struct.c
@@ -1,57 +1,57 @@
 #include<stdio.h>
-struct student
+struct studentlist
 {
      int roll;
      char name[20];
-     int marks[5];
+     int marks[10];
      int total;
      float percent;
 }
 stud[60];
 int main()
 {
-    int i,j,n;
+    int a,b,n;
     struct student s;
-    printf("Enter the number of Students : ");
+    printf("Enter the number of Students: ");
     scanf("%d",&n);
     printf("Subject 1 = Maths \nSubject 2 = English\nSubject 3 = Hindi\nSubject 4 = Physics\nSubject 1 = Chemistry\n");
-    for(i=0;i<n;i++)
+    for(a=0;a<n;a++)
     {
      	printf("\n");
         printf("Enter Details of Student %d ",i+1);
-        printf("\nEnter Roll-No. : ");
-        scanf("%d",&stud[i].roll);
-        printf("Enter Name : ");
-        scanf("%s",stud[i].name);
+        printf("\nEnter Roll-No: ");
+        scanf("%d",&stud[a].roll);
+        printf("Enter Name: ");
+        scanf("%s",stud[a].name);
           
-        for(j=0;j<5;j++)
+        for(b=0;b<5;b++)
         {
-            printf("Enter Marks of Subject %d : \n",j+1);
-            scanf("%d",&stud[i].marks[j]);
-            stud[i].total=stud[i].total+stud[i].marks[j] ;
-        	stud[i].percent=(float)((float)stud[i].marks[0]/(float)stud[i].total)*100;    
+            printf("Enter Marks of Subject %d : \n",b+1);
+            scanf("%d",&stud[a].marks[b]);
+            stud[a].total=stud[a].total+stud[a].marks[b] ;
+        	stud[a].percent=(float)((float)stud[a].marks[0]/(float)stud[a].total)*100;    
         }
           
     }
-    for(i=0;i<n;i++)
+    for(a=0;a<n;a++)
     {
-        for(j=i+1;j<5;j++)
+        for(b=a+1;b<5;b++)
         {
         	
-            if (stud[i].percent > stud[j].percent)
+            if (stud[a].percent > stud[b].percent)
             {
-                s=stud[i];
-                stud[i]=stud[j];
-                stud[j]=s;
+                s=stud[a];
+                stud[a]=stud[b];
+                stud[b]=s;
             }
         }
     }
     printf("Sorted Marks (According to Maths Mark)");
     printf("\n-----------------------------------\n");
     printf("\n ROLLNO     NAME  MATHS  \n");
-    for(i=0;i<n;i++)
+    for(a=0;a<n;a++)
     {
-    printf("\n  %d\t   %s\t   %.2f\t  ", stud[i].roll, stud[i].name, stud[i].percent);
+    printf("\n  %d\t   %s\t   %.2f\t  ", stud[a].roll, stud[a].name, stud[a].percent);
     }
     return 0;
 }
